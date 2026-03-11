3ec290572170d8367c8beac91544289a072f2436
hunglo <mcsherfey@yahoo.com>
2026-03-10 17:29:26 -0700

    hello world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+
+    commands
+        .spawn(Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        })
+        .with_child((
+            Text::new("Hello World"),
+            TextFont {
+                font_size: 64.0,
+                ..default()
+            },
+            TextColor(Color::WHITE),
+        ));
 }
